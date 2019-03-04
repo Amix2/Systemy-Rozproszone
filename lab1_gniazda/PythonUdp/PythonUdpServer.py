dfe2bfdea2a3525093627e7e8d1097637956ca94
Maciej Mucha <mmucha.00@gmail.com>
2019-03-04 15:47:31 +0100

    Initial commit

---
+++ lab1_gniazda/PythonUdp/PythonUdpServer.py
@@ -0,0 +1,19 @@
+import socket;
+
+serverPort = 9009
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+buff = []
+
+print('PYTHON UDP SERVER')
+
+while True:
+
+    buff, address = serverSocket.recvfrom(1024)
+    print("python udp server received msg: " + str(buff, 'cp1250'))
+    print(address)
+    msg = "response Python Udp!"
+    serverSocket.sendto(bytes(msg, 'cp1250'), address)
+
+
+
